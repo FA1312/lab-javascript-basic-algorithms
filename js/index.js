e6fc7639c95cdef50aed047e36a0ac973665ea67
FA1312 <faldrovandi83@gmail.com>
2021-10-11 11:34:53 +0200

    Done

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
- /*Iteration 1: Names and Input
+/*Iteration 1: Names and Input
 1.1 Create a variable `hacker1` with the driver's name.*/
 
 let hacker1 = "Mika";
@@ -34,13 +34,27 @@ console.log(hacker1.toUpperCase().split('').join(' '));
 
 /* or */
 
+let new_str = "";
+
 for (let i = 0; i < hacker1.length; i++) {
-  console.log(hacker1[i].toUpperCase() + " ")
+  new_str += hacker1[i] + " ";
+  console.log(new_str.toUpperCase());
 }
 
 /*3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ" */
 
 console.log(hacker2.split('').reverse().join(''));
+
+
+/* or */
+
+    let rev_str = "";
+    for( let i = hacker2.length-1 ;i >= 0 ;i--)
+    {
+      rev_str+= hacker2[i];
+    }
+    // return reverse string
+    console.log(rev_str);
 
 
 /*3.3 Depending on the lexicographic order of the strings, print:
@@ -64,13 +78,7 @@ Generate 3 paragraphs. Store the text in a variable type of string.
 Make your program count the number of words in the string.
 Make your program count the number of times the Latin word et appears.*/
 
-let baconIpsum = "Bacon ipsum dolor amet chuck jowl tail picanha sirloin boudin meatloaf ground round. Shank short loin hamburger 
-pork chop, shankle pork loin capicola ribeye meatloaf meatball porchetta.Boudin pork belly turducken cupim shank ribeye capicola 
-sausage venison picanha tri-tip chicken rump swine biltong. Beef ribs pork pork belly biltong jowl doner alcatra ham porchetta pig 
-corned beef pork chop pork loin. Beef ribs rump drumstick meatloaf pig boudin ball tip andouille. et et Porchetta tri-tip frankfurter 
-jerky. Beef ribs short loin tail, frankfurter strip steak biltong rump buffalo. Pork loin meatloaf tri-tip, corned beef frankfurter 
-picanha jowl ground round bresaola short ribs. Tongue beef tri-tip pork loin tenderloin capicola. T-bone leberkas swine pork chop 
-chuck."
+let baconIpsum = "Bacon ipsum dolor amet chuck jowl tail picanha sirloin boudin meatloaf ground round. Shank short loin hamburger pork chop, shankle pork loin capicola ribeye meatloaf meatball porchetta.Boudin pork belly turducken cupim shank ribeye capicola ausage venison picanha tri-tip chicken rump swine biltong. Beef ribs pork pork belly biltong jowl doner alcatra ham porchetta pig corned beef pork chop pork loin. Beef ribs rump drumstick meatloaf pig boudin ball tip andouille. et et Porchetta tri-tip frankfurter jerky. Beef ribs short loin tail, frankfurter strip steak biltong rump buffalo. Pork loin meatloaf tri-tip, corned beef frankfurter picanha jowl ground round bresaola short ribs. Tongue beef tri-tip pork loin tenderloin capicola. T-bone leberkas swine pork chop chuck."
 
 let prgSplit = baconIpsum.split(' ');
 console.log(prgSplit.length);
@@ -81,9 +89,7 @@ for ( let i = 0; i < prgSplit.length; i++) {
 }
 console.log(count);
 
-
-/*Bonus 2:
-Create a new variable phraseToCheck and have it contain some string value. Write a code that will check if the value we assigned to this variable is a Palindrome. Here are some examples of palindromes:
+/* Write a code that will check if the value we assigned to this variable is a Palindrome. Here are some examples of palindromes:
 
 "A man, a plan, a canal, Panama!"
 "Amor, Roma"
@@ -94,7 +100,17 @@ Create a new variable phraseToCheck and have it contain some string value. Write
 "put it up"
 "Was it a car or a cat I saw?" and "No 'x' in Nixon". */
 
-
+let str = "step on no pets";
+let reverse = "";
+for (let i = str.length - 1; i >= 0; i--) {
+  reverse += str[i];
+  reverse = reverse.toLowerCase()
+}
+if (reverse == str.toLowerCase()) {
+  console.log("passed string is palindrome ");
+} else {
+  console.log("passed string is not palindrome");
+}
 
 
 /*Hint: If you use Google to help you to find solution to this iteration, you might run into some solutions that use advanced string or array methods (such as join(), reverse(), etc.). However, try to apply the knowledge you currently have since you can build pretty nice solution with just using for loop, if-else statements with some break and continue... Just sayin' smiley*/
